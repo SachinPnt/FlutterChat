64a702af09ad988f2a1c5a334c47a6e9ce1c46d9
SachinPnt <156996753+SachinPnt@users.noreply.github.com>
2024-11-05 15:55:29 +0530

    Add files via upload
    first commit for MOJ Lite

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "652864078"
+    serviceWorkerVersion: "2318748268"
   }
 });
