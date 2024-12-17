9f0276b233442734c090a2fab2070a68497e6b23
SachinPnt <156996753+SachinPnt@users.noreply.github.com>
2024-12-17 15:19:00 +0530

    Add files via upload
    for MOJ lite Chat with new features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2318748268"
+    serviceWorkerVersion: "3816212139"
   }
 });
